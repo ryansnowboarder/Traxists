a5e114daa47b9e85fa391465149d7c3f4c546cf4
ryansnowboarder <ryansnowboarder@gmail.com>
2015-08-08 20:49:19 -0400

    Fixed bug caused by calling Parse asynchronously in an AsyncTask.

--- app/src/main/java/com/xchange_place/traxists/traxists/EnterUsernameAndPasswordFragment.java
+++ app/src/main/java/com/xchange_place/traxists/traxists/EnterUsernameAndPasswordFragment.java
@@ -11,12 +11,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-
-import java.util.List;
 
 /**
  * Created by Ryan Fletcher on 8/3/2015.
@@ -27,6 +24,8 @@ import java.util.List;
  *
  */
 public class EnterUsernameAndPasswordFragment extends Fragment {
+
+    private static final String TAG = "EnterUsrnmAndPwdFrag";
 
     // GUI objects found in fragment_enter_username_and_password
     private EditText username_edittext;
@@ -66,7 +65,7 @@ public class EnterUsernameAndPasswordFragment extends Fragment {
         login_setup_next_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new VerifyUsername().doInBackground();
+                new VerifyUsername().execute();
             }
         });
 
@@ -85,21 +84,17 @@ public class EnterUsernameAndPasswordFragment extends Fragment {
     }
 
     // the aforementioned AsyncTask
-    private class VerifyUsername extends AsyncTask<Void, String, Void> {
+    private class VerifyUsername extends AsyncTask<Void, Void, Boolean> {
 
         // while Parse is being queried, a ProgressDialog is displayed
         private ProgressDialog pd;
 
-        // the username to be verified
-        private String username;
-
-        // a flag to store whether or not the latest username entered is available
-        private boolean isUsernameAvailable;
+        // a flag to store whether or not the desired username is currently available
+        public boolean isUsernameAvailable;
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            username = username_edittext.getText().toString();
             // setup the ProgressDialog
             pd = new ProgressDialog(getActivity());
             pd.setTitle("Please Wait...");
@@ -112,39 +107,34 @@ public class EnterUsernameAndPasswordFragment extends Fragment {
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                // create a ParseQuery instance to check the availability of the username
-                // that the user-client has entered into the EditText in the GUI
-                ParseQuery<ParseObject> query = ParseQuery.getQuery("Creators");
-                query.whereEqualTo("username", username);
-                query.findInBackground(new FindCallback<ParseObject>() {
-                    public void done(List<ParseObject> objects, ParseException e) {
-                        if (e == null) {
-                            // no errors have occured, meaning that the username has been found
-                            // in the Parse MBAAS table and is not available for usage
-                            isUsernameAvailable = false;
-                        } else {
-                            // if an error occurs, the username has not been found in the
-                            // Parse MBAAS and is currently available for usage
-                            isUsernameAvailable = true;
-                        }
-                    }
-                });
-            } catch (Exception e) {
-                // if there is an exception, treat the query as if an error
-                // had occurred
-                isUsernameAvailable = false;
-            }
-            return null;
+        protected Boolean doInBackground(Void... params) {
+            // create a ParseQuery instance to check the availability of the username
+            // that the user-client has entered into the EditText in the GUI
+            ParseQuery<ParseObject> query;
+                if (MainActivity.getUser().getAccType() == 0)
+                    query = ParseQuery.getQuery("Creators");
+                else if (MainActivity.getUser().getAccType() == 1)
+                    query = ParseQuery.getQuery("Admins");
+                else
+                    query = ParseQuery.getQuery("Users");
+
+            query.whereEqualTo("username", username_edittext.getText().toString());
+                try {
+                    if (query.find().size() > 0)
+                        return false;
+                    else
+                        return true;
+                } catch (ParseException e) {
+                    return false;
+                }
         }
 
         @Override
-        protected void onPostExecute(Void result) {
+        protected void onPostExecute(Boolean result) {
             super.onPostExecute(result);
             // dismiss the ProgressDialog instance
             pd.dismiss();
-            if (!isUsernameAvailable) {
+            if (!result) {
                 // if the username is not available, create a Toast warning
                 // the user that the username is already taken and to enter
                 // a new username
@@ -154,13 +144,12 @@ public class EnterUsernameAndPasswordFragment extends Fragment {
                         Toast.LENGTH_SHORT).show();
             } else {
                 // if the username is available, check the password for proper length
-                String password = password_edittext.getText().toString();
-                if (password.length() > 10) {
+                if (password_edittext.getText().length() > 10) {
                     // if the password is of proper length, store the username
                     // and password configuration in the User instance of the
                     // MainActivity
-                    MainActivity.getUser().setUsername(username);
-                    MainActivity.getUser().setPassword(password);
+                    MainActivity.getUser().setUsername(username_edittext.getText().toString());
+                    MainActivity.getUser().setPassword(password_edittext.getText().toString());
                     ReplaceWithConfirmPasswordFragmentAndAddToBackstack();
                 } else {
                     Toast.makeText(
