42afad3a10f4603e6598a9467c391acafbad3213
ryansnowboarder <ryansnowboarder@gmail.com>
2015-08-09 23:56:17 -0400

    Created this fragment that is visible by only Admin accounts.

---
+++ app/src/main/java/com/xchange_place/traxists/traxists/SettingsFragment.java
@@ -0,0 +1,11 @@
+package com.xchange_place.traxists.traxists;
+
+/**
+ * Created by Ryan Fletcher on 8/9/2015.
+ *
+ * This fragment is only visible to Admin accounts. In this fragment, the Admin user-client
+ * is able to add or delete User accounts from the list of User accounts that they are subscribed
+ * to receive location updates from
+ */
+public class SettingsFragment {
+}
